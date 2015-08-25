a6a56bc3ce83a81dd376a963fccd3911a0ece835
王刚 <wanggang@zxisl.com>
2015-08-25 18:25:25 +0800

    初始化项目

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# ERP项目
+
+> 基于SSH实现。  
+
+
+## 生成pojo和hbm文件
+
+> 在hibernate.reveng.xml配置好需要生成hibernate需要的pojo和hbm的表名后, 执行 
+
+`mvn hibernate3:hbm2hbmxml hibernate3:hbm2java`
+
---
+++ src/main/java/com/palmelf/erp/util/ExceptionUtil.java
@@ -0,0 +1,47 @@
+package com.palmelf.erp.util;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 类功能说明 TODO:Exception工具类
+ * 类修改者	修改日期
+ * 修改说明
+ * <p>Title: BaseService.java</p>
+ * <p>Description:福产流通科技</p>
+ * <p>Copyright: Copyright (c) 2012</p>
+ * <p>Company:福产流通科技</p>
+ * @author shouyin 756514656@qq.com
+ * @date 2013-4-19 下午03:18:05
+ * @version V1.0
+ */
+public class ExceptionUtil {
+
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionUtil.class);
+
+	/**
+	 * 返回错误信息字符串
+	 *
+	 * @param ex
+	 *            Exception
+	 * @return 错误信息字符串
+	 */
+	public static String getExceptionMessage(Exception ex) {
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		ex.printStackTrace(pw);
+		String errorMessage = sw.toString();
+		pw.close();
+		try {
+			sw.close();
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+		}
+		return errorMessage;
+	}
+
+}
